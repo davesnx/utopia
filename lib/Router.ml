9afee8cdb9ea0edf5986721c30b1517165ae0bd6
David Sancho Moreno <dsnxmoreno@gmail.com>
2024-03-31 18:45:44 +0200

    Format with oformat

--- lib/Router.ml
+++ lib/Router.ml
@@ -1,15 +1,19 @@
-module type Page =
-  sig
-    val path: string
-    val make: ?key: string -> unit -> React.element
-  end
+module type Page = sig
+  val path : string
+  val make : ?key:string -> unit -> React.element
+end
+
+(* This should not live here *)
+let make path element : (module Page) =
+  (module struct
+    let path = path
+    let make ?key:_ () = element ()
+  end)
 
 let pages = ref []
-
-let register (module P: Page) =
-  pages := (module P:Page) :: !pages
+let register (module P : Page) = pages := (module P : Page) :: !pages
 
 let get_pages () : (module Page) list =
   match !pages with
-  | []-> failwith "There are no registered Pages"
+  | [] -> failwith "There are no registered Pages"
   | pages -> pages
